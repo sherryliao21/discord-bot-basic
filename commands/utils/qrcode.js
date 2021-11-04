34ed2a8d737d0574b9a31685ddd93f94a9da9c20
Sherry Liao <sherry.liao@pioneerm.com>
2021-11-04 15:20:10 +0800

    fix error handling for qrcode generating

--- commands/utils/qrcode.js
+++ commands/utils/qrcode.js
@@ -8,12 +8,11 @@ module.exports = {
     execute(message, args) {
         if (message.author.bot) return
         if (!args.length) {
-            message.channel.send(`Please provide an URL in the arguments\nEx:   #qrcode https://example.com`)
+            return message.channel.send(`Please provide an URL in the arguments\nEx:   #qrcode https://example.com`)
         }
 
         let url, size, colorHex
         [url, size, colorHex] = [args[0], args[1] || 180, args[2] || 000000]
-        const encodeMethod = '&choe=UTF-8'
 
         const validateResult = validate({ website: url }, {
             website: {
@@ -24,8 +23,7 @@ module.exports = {
         if (typeof validateResult !== 'undefined') {
             return message.reply('that\'s not a valid URL!');
         } else {
-            const rootUrl = `https://chart.googleapis.com/chart?cht=qr&chco=${colorHex}&chs=${size}x${size}&chl=`
-            const finalUrl = rootUrl + url + encodeMethod
+            const rootUrl = `https://chart.googleapis.com/chart?cht=qr&chco=${colorHex}&chs=${size}x${size}&chl=${url}&choe=UTF-8`
 
             const qrcodeEmbeded = new MessageEmbed()
                 .setColor(colorHex || 'RANDOM')
@@ -37,7 +35,7 @@ module.exports = {
                     name: 'Original url: ',
                     value: url
                 })
-                .setImage(finalUrl)
+                .setImage(rootUrl)
                 .setTimestamp()
                 
             message.reply(`You\'re good to go!\nHere is your QRcode: `)
